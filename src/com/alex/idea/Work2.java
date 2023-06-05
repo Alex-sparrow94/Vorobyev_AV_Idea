c18697648600a365b6ba2f19d9bc6037dc963305
Alex-sparrow94 <taken94@yandex.ru>
2023-06-05 22:08:44 +0300

    add my Second Java HomeWork

---
+++ src/com/alex/idea/Work2.java
@@ -0,0 +1,10 @@
+package com.alex.idea;
+
+public class Work2 {
+    public static void main(String[] args) {
+        double d=123.12345678901234;
+        System.out.println(d);
+        byte b=127;
+        System.out.println(b);
+    }
+}
